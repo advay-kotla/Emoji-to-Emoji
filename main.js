3f65e77bec6565fabd607291e7551af35011f1cc
advay-kotla <65086329+advay-kotla@users.noreply.github.com>
2021-05-03 17:50:59 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -39,39 +39,39 @@ function take_snapshot() {
 
     function gotResult(error, results) {
         if (error) {
-            console.error(error)
+            console.error(error);
         } 
         else {
             console.log(results)
-            document.getElementById("result_emotion_name").innerHTML = results[0].label
+            document.getElementById("result_emotion_name1").innerHTML = results[0].label
             document.getElementById("result_emotion_name2").innerHTML = results[1].label
             prediction_1 = results[0].label
             prediction_2 = results[1].label
             speak();
             if(result[0].label == "happy")
             {
-                document.getElementById("update_emoji").innerHTML ="&#128522"
+                document.getElementById("update_emoji1").innerHTML ="&#128522"
             }
             if(result[0].label == "sad")
             {
-                document.getElementById("update_emoji").innerHTML ="&#128532"
+                document.getElementById("update_emoji1").innerHTML ="&#128532"
             }
             if(result[0].label == "angry")
             {
-                document.getElementById("update_emoji").innerHTML ="&#128548"
+                document.getElementById("update_emoji1").innerHTML ="&#128548"
             }
 
             if(result[1].label == "happy")
             {
-                document.getElementById("update_emoji").innerHTML ="&#128522"
+                document.getElementById("update_emoji2").innerHTML ="&#128522"
             }
             if(result[1].label == "sad")
             {
-                document.getElementById("update_emoji").innerHTML ="&#128532"
+                document.getElementById("update_emoji2").innerHTML ="&#128532"
             }
             if(result[1].label == "angry")
             {
-                document.getElementById("update_emoji").innerHTML ="&#128548"
+                document.getElementById("update_emoji2").innerHTML ="&#128548"
             }
 
         }
